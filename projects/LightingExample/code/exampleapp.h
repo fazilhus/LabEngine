27936b8ef85a7959b533841d78e05fe8c44f2ae4
fazkhu <khusnutdinov.fazil@gmail.com>
2024-09-23 02:32:40 +0200

    forgot to add a new project

---
+++ projects/LightingExample/code/exampleapp.h
@@ -0,0 +1,49 @@
+#pragma once
+//------------------------------------------------------------------------------
+/**
+	Application class used for example application.
+	
+	(C) 2015-2022 Individual contributors, see AUTHORS file
+*/
+//------------------------------------------------------------------------------
+#include "core/app.h"
+#include "render/window.h"
+#include "render/mesh.h"
+#include "render/shader.h"
+#include "render/grid.h"
+#include "render/camera.h"
+#include "math/mat4.h"
+#include "render/node.h"
+
+#include <memory>
+
+namespace Example {
+
+	class ImGuiExampleApp : public Core::App {
+	public:
+		/// constructor
+		ImGuiExampleApp();
+		/// destructor
+		~ImGuiExampleApp();
+
+		/// open app
+		bool Open();
+		/// clone app
+		void Close();
+		/// run app
+		void Run();
+		void HandleInput();
+
+	private:
+		std::shared_ptr<Resource::Shader> shader;
+		//Resource::Mesh mesh;
+		Resource::GraphicsNode cube;
+		Render::Grid* grid;
+		Render::Camera* camera;
+		Display::Window* window;
+		float time;
+		float prev_time;
+		float dt;
+	};
+
+} // namespace Example
