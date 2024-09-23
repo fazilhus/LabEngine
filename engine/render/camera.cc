f0c2a32710df91e6f52c12cf5d2f4d6a37a3f953
fazkhu <khusnutdinov.fazil@gmail.com>
2024-09-23 13:47:57 +0200

    adjusted camera controls

--- engine/render/camera.cc
+++ engine/render/camera.cc
@@ -10,7 +10,7 @@ namespace Render {
 	
 	Camera::Camera(const float fovy, const float aspect, const float near, const float far)
 		: pos({ 0, 0, 0 }), at(Math::vec3{ 0, 0, 1 }), up({ 0, 1, 0 }),
-		yaw(-90.0f), pitch(0), speed(25.0f), sens(0.15f) {
+		yaw(0.0f), pitch(0), speed(25.0f), sens(0.15f) {
 		perspective = Math::perspective(fovy, aspect, near, far);
 		UpdateView();
 	}
--- projects/LightingExample/code/exampleapp.cc
+++ projects/LightingExample/code/exampleapp.cc
@@ -68,16 +68,16 @@ namespace Example {
 			prev_time = 0;
 
 			this->cube = Resource::GraphicsNode(
-				"../projects/GraphicsNodeExample/res/meshes/bunny.obj",
-				"../projects/GraphicsNodeExample/res/textures/minecraft-dirt.png",
-				"../projects/GraphicsNodeExample/res/shaders/vertex.glsl",
-				"../projects/GraphicsNodeExample/res/shaders/fragment.glsl"
+				"../projects/LightingExample/res/meshes/bunny.obj",
+				"../projects/LightingExample/res/textures/minecraft-dirt.png",
+				"../projects/LightingExample/res/shaders/vertex.glsl",
+				"../projects/LightingExample/res/shaders/fragment.glsl"
 			);
 			this->cube.transform *= Math::translate({0, 0, 0});
 
 			this->camera = new Render::Camera(0.5f, 4.0f / 3.0f, 0.01f, 100.0f);
-			this->camera->SetCameraPosition({0.0f, 2.0f, -2.0f});
-			this->camera->SetSpeed(10.0f);
+			this->camera->SetCameraPosition({0.0f, 0.0f, -2.0f});
+			this->camera->SetSpeed(7.5f);
 			this->camera->SetSens(0.05f);
 
 			this->grid = new Render::Grid();
