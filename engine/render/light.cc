f410def58096788950ff179854a519437d179cf0
fazkhu <khusnutdinov.fazil@gmail.com>
2024-09-26 18:01:31 +0200

    added spot light class

--- engine/render/light.cc
+++ engine/render/light.cc
@@ -1,6 +1,8 @@
 #include "config.h"
 #include "light.h"
 
+#include "math/math.h"
+
 namespace Render {
 
 	Light::Light()
@@ -27,6 +29,15 @@ namespace Render {
 	PointLight::PointLight(const Math::vec3& pos, const Math::vec3& a,
 		const Math::vec3& d, const Math::vec3& s, const Math::vec3& attenuation)
 		: pos(pos), Light(a, d, s), attenuation(attenuation) {
+	}
+
+	SpotLight::SpotLight()
+		: pos(0.0f), dir({0.0f, -1.0f, 0.0f}), cutoff(Math::toRad(30.0f)), Light(), attenuation({ 1.0f, 0.0f, 0.0f }) {
+	}
+
+	SpotLight::SpotLight(const Math::vec3& pos, const Math::vec3& dir, float32 cutoff, const Math::vec3& a,
+		const Math::vec3& d, const Math::vec3& s, const Math::vec3& attenuation)
+		: pos(pos), dir(dir), cutoff(cutoff), Light(a, d, s), attenuation(attenuation) {
 	}
 
 } // Render
--- engine/render/light.h
+++ engine/render/light.h
@@ -59,12 +59,39 @@ namespace Render {
 		~PointLight() = default;
 
 		inline void SetPos(const Math::vec3& pos) { this->pos = pos; }
-
 		inline void SetAttenuation(const Math::vec3& attenuation) { this->attenuation = attenuation; }
 
 		inline const Math::vec3& GetPos() const { return pos; }
 		inline Math::vec3& GetPos() { return pos; }
+		inline const Math::vec3& GetAttenuation() const { return attenuation; }
+		inline Math::vec3& GetAttenuation() { return attenuation; }
+	};
 
+	class SpotLight : public Light {
+	private:
+		Math::vec3 pos;
+		Math::vec3 dir;
+		float32 cutoff;
+
+		Math::vec3 attenuation;
+
+	public:
+		SpotLight();
+		SpotLight(const Math::vec3& pos, const Math::vec3& dir, float32 cutoff, const Math::vec3& a,
+			const Math::vec3& d, const Math::vec3& s, const Math::vec3& attenuation);
+		~SpotLight() = default;
+
+		inline void SetPos(const Math::vec3& pos) { this->pos = pos; }
+		inline void SetDirection(const Math::vec3& dir) { this->dir = dir; }
+		inline void SetCutoffAngle(float32 angle) { this->cutoff = angle; }
+		inline void SetAttenuation(const Math::vec3& attenuation) { this->attenuation = attenuation; }
+
+		inline const Math::vec3& GetPos() const { return pos; }
+		inline Math::vec3& GetPos() { return pos; }
+		inline const Math::vec3& GetDirection() const { return dir; }
+		inline Math::vec3& GetDirection() { return dir; }
+		inline float32 GetCutoffAngle() const { return cutoff; }
+		inline float32 GetCutoffAngle() { return cutoff; }
 		inline const Math::vec3& GetAttenuation() const { return attenuation; }
 		inline Math::vec3& GetAttenuation() { return attenuation; }
 	};
