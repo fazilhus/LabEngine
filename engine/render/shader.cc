27bfaaa288b74ed852046e303165a617f338636f
fazkhu <khusnutdinov.fazil@gmail.com>
2024-09-27 13:36:24 +0200

    no comments...

--- engine/render/shader.cc
+++ engine/render/shader.cc
@@ -175,7 +175,7 @@ namespace Resource {
 	}
 
 	GLuint Shader::GetOrUpdateUniformLoc(const std::string& name) {
-		if (uniformLoc.contains(name)) {
+		if (!uniformLoc.contains(name)) {
 		//if (uniformLoc.find(name) == uniformLoc.end()) {
 			uniformLoc[name] = glGetUniformLocation(handle, name.c_str());
 		}
